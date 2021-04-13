5ab17f7fb5c6c0a2b8a88883a9f3a8cd3ecf8027
Sebastián Cardozo <sebastian.cardozo97@gmail.com>
2021-04-13 11:37:46 -0500

    New implementations

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,4 +1,8 @@
+const { v4: uuidv4 } = require("uuid");
+const ValidationError = require("../../../errors/ValidationError");
+
 const TABLE = "users";
+const CTRL_NAME = "messageController";
 
 module.exports = (injectedStore) => {
     let store = injectedStore;
@@ -10,7 +14,40 @@ module.exports = (injectedStore) => {
         return store.list(TABLE);
     };
 
+    const get = (id) => {
+        return store.get(TABLE, id);
+    };
+
+    const add = (name) => {
+        return new Promise((resolve, reject) => {
+            let errors = [];
+            if (!name) {
+                console.error(`[${CTRL_NAME}] No name`);
+                errors.push({ name: "Name is required" });
+            }
+            if (errors.length) {
+                return reject(new ValidationError(errors));
+            }
+
+            let newUser = {
+                id: uuidv4(),
+                name,
+            };
+
+            store
+                .upsert(TABLE, newUser)
+                .then((user) => {
+                    resolve(user);
+                })
+                .catch((e) => {
+                    reject(e);
+                });
+        });
+    };
+
     return {
         list,
+        get,
+        add,
     };
 };
--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,16 +1,41 @@
 const express = require("express");
+const ValidationError = require("../../../errors/ValidationError");
 const response = require("../../../network/response");
 const router = express.Router();
 const Controller = require("./index");
 
 router.get("/", (req, res) => {
-    const users = Controller.list();
-    response.success(req, res, users);
+    Controller.list()
+        .then((users) => {
+            response.success(req, res, users);
+        })
+        .catch((e) => {
+            response.error(req, res, "Internal server error", e);
+        });
 });
 
 router.get("/:id", (req, res) => {
-    const user = Controller.get(req.params.id);
-    response.success(req, res, user);
+    Controller.get(req.params.id)
+        .then((user) => {
+            response.success(req, res, user);
+        })
+        .catch((e) => {
+            response.error(req, res, "Internal server error", e);
+        });
+});
+
+router.post("/", (req, res) => {
+    Controller.add(req.body.name)
+        .then((user) => {
+            response.success(req, res, user);
+        })
+        .catch((e) => {
+            if (e instanceof ValidationError) {
+                response.fail(req, res, e.data, 422);
+            } else {
+                response.error(req, res, "Internal server error", e);
+            }
+        });
 });
 
 module.exports = router;
